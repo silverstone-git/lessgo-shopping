8149e54b8e3ef7cda382bc6c14a6d7027035c9e7
silverstone-git <silverstone965@gmail.com>
2023-06-05 15:45:01 +0530

    made and populated a new map each time plus/minus is clicked on item card to update state properly and instantly thus fixing the delay bug

--- src/ItemMaster.tsx
+++ src/ItemMaster.tsx
@@ -17,12 +17,9 @@ async function getItems(jwtToken: String) {
     const resJ = await res.json();
     const data = resJ.itemList;
 
-    // console.log(`data list is: ${data}`);
     itemList = data.map((el: Object) => {
-        console.log(el);
         return el as Item;
     });
-    // console.log(`item list is: ${itemList}`);
     return itemList;
 }
 
@@ -34,42 +31,56 @@ function ItemMaster(props: any) {
     const initList: Array<Item> = [];
     const [listOfItems, setListOfItems] = useState(initList);
 
+
     const newItemCount: Map<string, number> = new Map();
     const [noOfItems, setNoOfItems] = useState(newItemCount);
 
-    setInterval(function() {
-        getItems(props.jwtToken).then((val) => {setListOfItems(val)});
-    }, 10000);
+    // setInterval(function() {
+    //     getItems(props.jwtToken).then((val) => {setListOfItems(val)});
+    // }, 10000);
 
 
     function changeCount(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        var breh = noOfItems;
+        // JSON.parse(JSON.stringify(myObj));
+        var breh: Map<string, number> = new Map();
+        const newArr = Array.from(noOfItems.entries());
+        for( var i = 0; i < newArr.length; i ++) {
+            if(newArr[i][1] > 0) {
+                breh.set(newArr[i][0], newArr[i][1]);
+            }
+        }
         const thisID = e.currentTarget.value;
-        const thisCount = noOfItems.get(thisID);
+        const thisCount = noOfItems.get(thisID) ? noOfItems.get(thisID) : 0;
         if(e.currentTarget.classList.contains('addButton')) {
             breh.set(thisID, thisCount ? thisCount + 1 : 1)
         } else if(e.currentTarget.classList.contains('subtractButton')) {
-            breh.set(thisID, thisCount ? thisCount - 1 : 0);
+            if(thisCount == 0 || thisCount == undefined) {
+                // pass
+            } else if(thisCount == 1) {
+                breh.delete(thisID);
+            } else {
+                breh.set(thisID, thisCount - 1);
+            }
         }
-        // console.log(breh.keys());
-        // console.log(breh.values());
+        // console.log(`new map is :`);
+        // console.log(breh.entries());
         setNoOfItems(breh);
     }
 
     function ItemCard(props: any) {
         return (
-        <div id={props.thisId} className=' w-1/2 md:w-1/4 overflow-hidden p-8'>
+        <div id={props.thisId} className=' w-full sm:w-1/2 md:w-1/3 lg:w-1/4 overflow-hidden p-8'>
             <div className=' border rounded border-slate-500 flex flex-col justify-center'>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"></img>
                 <div className="flex justify-between h-12 items-center gap-2">
-                    <div>{props.itemName} {props.priceRs}</div>
-                    <div className=' flex'>
-                        <button className='addButton' value={props.thisId} onClick={(e) => changeCount(e)}>
-                            <FontAwesomeIcon icon={icon({name: 'plus', style: 'solid'})} />
-                        </button>
-                        <div>{props.thisCount}</div>
-                        <button className='subtractButton' value={props.thisId} onClick={(e) => changeCount(e)}>
+                    <div className='ml-5 py-3'>{props.itemName} {props.priceRs}</div>
+                    <div className=' flex items-center'>
+                        <button className='subtractButton p-5' value={props.thisId} onClick={(e) => changeCount(e)}>
                             <FontAwesomeIcon icon={icon({name: 'minus', style: 'solid'})} />
+                        </button>
+                        <div className='px-4'>{props.thisCount}</div>
+                        <button className='addButton p-5' value={props.thisId} onClick={(e) => changeCount(e)}>
+                            <FontAwesomeIcon icon={icon({name: 'plus', style: 'solid'})} />
                         </button>
 
                     </div>
@@ -100,14 +111,12 @@ function ItemMaster(props: any) {
         getItems(props.jwtToken).then((val) => {setListOfItems(val)});
 }, [])
     
-    console.log(noOfItems.keys());
-    console.log(noOfItems.values());
     return (
-        <div className='flex justify-center items-center h-screen w-full text-slate-800 bg-slate-100 dark:text-slate-100 dark:bg-slate-800'>
-            <div className='flex w-full flex-wrap items-center justify-center'>
+        <div className='flex items-center h-screen w-full text-slate-800 bg-slate-100 dark:text-slate-100 dark:bg-slate-800'>
+            <div className='mt-[20vh] flex w-full flex-wrap items-center justify-center bg-slate-100 dark:bg-slate-800'>
                 <Items {...{"listOfItems": listOfItems, "noOfItems": noOfItems}} />
             </div>
-        </div>
+            </div>
     );
 }
 
